b56d4e6026d2a220fb81b6bf6737c2b2f78534a9
BrianKip2000 <kipchirchirbrian559@gmail.com>
2024-03-18 19:26:32 +0300

    This code!

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """ A function that etrieves all elements in a list"""
 def element_at(my_list, idx):
-        if num < 0:
+        if idx < 0:
             return None
-        elif num > len(my_list):
+        elif idx > len(my_list):
             return None
         else:
             return my_list[idx]
