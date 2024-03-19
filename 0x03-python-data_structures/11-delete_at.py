59c029172eacd2a6b603684c133b3cb77135194e
BrianKip2000 <kipchirchirbrian559@gmail.com>
2024-03-19 19:15:18 +0300

    Delete at

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -6,13 +6,13 @@ def delete_at(my_list=[], idx=0):
         return my_list
 
     # Initialize an empty list to store the modified list
-    modified_list = []
+    my_list = []
 
     # Iterate through the elements of the original list
     for i in range(len(my_list)):
         # Skip the element at the specified index (idx)
         if i != idx:
             # Append elements other than the one at idx to the modified list
-            modified_list.append(my_list[i])
+            my_list.append(my_list[i])
 
-    return modified_list
+    return my_list
