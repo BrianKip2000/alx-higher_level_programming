0aba4614e8427445e37083ae801f256974b546d9
BrianKip2000 <kipchirchirbrian559@gmail.com>
2024-03-19 13:58:18 +0300

    Boring

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    my_string = my_string.remove('c', 'C')
-    print(my_string)
+    result = ''
+    for char in my_string:
+        if char != 'c' and char != 'C':
+            result += char
+    return result
