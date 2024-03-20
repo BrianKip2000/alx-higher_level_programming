57f4871d8224c51302f8bd7ac7c9752acd792ba9
BrianKip2000 <kipchirchirbrian559@gmail.com>
2024-03-20 08:40:06 +0300

    Argument

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import sys
 
+
 def main():
     args = sys.argv[1:]  # Exclude the script name from arguments
     num_args = len(args)
@@ -16,6 +17,7 @@ def main():
     # Print each argument
     for i, arg in enumerate(args, start=1):
         print(f"{i}: {arg}")
+
 
 if __name__ == "__main__":
     main()
