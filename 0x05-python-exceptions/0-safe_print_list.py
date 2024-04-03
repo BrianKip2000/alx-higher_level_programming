9b15dece3790f640a1e85a68cc30c3e9d41b5e8d
BrianKip2000 <kipchirchirbrian559@gmail.com>
2024-04-03 13:47:42 +0300

    try

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,7 +6,7 @@ def safe_print_list(my_list=[], x=0):
     for i in range(0, x):
         # find the items x in my list
         try:
-            print("{}".format(my_list[i]), end=" ")
+            print("{}".format(my_list[i]), end="")
             printed += 1
         except ValueError:
             continue
