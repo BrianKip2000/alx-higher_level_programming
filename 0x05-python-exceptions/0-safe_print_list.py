47a783f82e009809e9980ac01f87c0384affe5cc
BrianKip2000 <kipchirchirbrian559@gmail.com>
2024-04-03 13:28:45 +0300

    Safe print list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -9,7 +9,7 @@ def safe_print_list(my_list=[], x=0):
         try:
             print(":d".format(my_list[elements]), end=" ")
             elements += 1  # type: ignore
-        except:
+        except ValueError:
             continue
         print()
         return(printed)
