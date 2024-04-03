800371fc7bdd2ec60b6054f18fc5b0a80ffc4a18
BrianKip2000 <kipchirchirbrian559@gmail.com>
2024-04-04 00:38:30 +0300

    raise namerror

--- 0x05-python-exceptions/6-raise_exception_msg.py
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def raise_exception_msg(message=""):
-    try:
-        geek = input()
-        print(geek)
-    except NameError:
-        return message
+    raise NameError(message)
