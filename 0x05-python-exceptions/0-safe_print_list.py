6bd4b65c8a384e5056ec594579379c276b172cfa
BrianKip2000 <kipchirchirbrian559@gmail.com>
2024-04-03 13:39:01 +0300

    Corrected without i

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,15 +1,14 @@
-#!usr/bin/python3
+#!/usr/bin/python3
 # a python file that prints the items in a list
 def safe_print_list(my_list=[], x=0):
-    i = 0
     printed = 0
 
     for i in range(0, x):
         # find the items x in my list
         try:
-            print(":d".format(my_list[i]), end=" ")
-            elements += 1  # type: ignore
+            print("{}".format(my_list[i]), end=" ")
+            printed += 1
         except IndexError:
             continue
-        print()
-        return(printed)
+    print()
+    return printed
