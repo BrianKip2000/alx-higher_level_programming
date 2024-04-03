0bbe85d50da65944a547fdc5df9e77730c674a30
BrianKip2000 <kipchirchirbrian559@gmail.com>
2024-04-04 00:27:07 +0300

    exception raised

--- 0x05-python-exceptions/5-raise_exception.py
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def raise_exception():
     try:
-        #Perfom an impossible operation
-        return None +2
+        return None + 2
     except TypeError:
         raise TypeError("Mission Impossible")
