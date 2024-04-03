62055aceb129b66c315a72a7fcd50aab53d8a1ac
BrianKip2000 <kipchirchirbrian559@gmail.com>
2024-04-04 00:58:53 +0300

    execute a function safely

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,10 @@
+def safe_function(fct, *args):
+    try:
+        # Call the function with the provided arguments
+        result = fct(*args)
+        return result
+    except Exception as e:
+        # If any exception occurs, print the error message to stderr
+        import sys
+        print("Exception: {}".format(e), file=sys.stderr)
+        return None
