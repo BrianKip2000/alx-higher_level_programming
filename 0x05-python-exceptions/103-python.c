be3811455def7f64fc2fdf682f8a09680ce1331d
BrianKip2000 <kipchirchirbrian559@gmail.com>
2024-04-04 07:18:50 +0300

    cython

---
+++ 0x05-python-exceptions/103-python.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+
+/**
+ * print_python_list - Print information about Python lists
+ * @p: PyObject pointer to a Python object
+ */
+void print_python_list(PyObject *p) {
+    Py_ssize_t size, i;
+    PyObject *item;
+
+    if (!PyList_Check(p)) {
+        printf("[ERROR] Invalid List Object\n");
+        return;
+    }
+
+    size = PyList_Size(p);
+    printf("[*] Python list info\n");
+    printf("[*] Size of the Python List = %ld\n", size);
+    printf("[*] Allocated = %ld\n", ((PyListObject *)p)->allocated);
+
+    for (i = 0; i < size; i++) {
+        item = PyList_GetItem(p, i);
+        printf("Element %ld: %s\n", i, Py_TYPE(item)->tp_name);
+    }
+}
+
+/**
+ * print_python_bytes - Print information about Python bytes
+ * @p: PyObject pointer to a Python object
+ */
+void print_python_bytes(PyObject *p) {
+    Py_ssize_t size, i;
+    char *str;
+
+    if (!PyBytes_Check(p)) {
+        printf("[ERROR] Invalid Bytes Object\n");
+        return;
+    }
+
+    size = PyBytes_Size(p);
+    str = PyBytes_AsString(p);
+
+    printf("[.] bytes object info\n");
+    printf("  size: %ld\n", size);
+    printf("  trying string: %s\n", str);
+    printf("  first 10 bytes:");
+
+    for (i = 0; i < size && i < 10; i++)
+        printf(" %02x", str[i] & 0xff);
+
+    printf("\n");
+}
+
+/**
+ * print_python_float - Print information about Python float
+ * @p: PyObject pointer to a Python object
+ */
+void print_python_float(PyObject *p) {
+    if (!PyFloat_Check(p)) {
+        printf("[ERROR] Invalid Float Object\n");
+        return;
+    }
+
+    printf("[.] float object info\n");
+    printf("  value: %f\n", ((PyFloatObject *)p)->ob_fval);
+}
