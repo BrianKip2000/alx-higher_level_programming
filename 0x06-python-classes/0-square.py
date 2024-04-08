136e8c1462a9ada9c5ff49cb5ebb0fbf06927abc
BrianKip2000 <kipchirchirbrian559@gmail.com>
2024-04-08 23:39:56 +0300

    Square

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-# Define an empty class
+""" Define an empty class """
+
+
 class Square:
-    # The class passes at since there are no class attributes
+    """Class Square defined """
     pass
