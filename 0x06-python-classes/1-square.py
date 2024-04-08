4b44b0cc3266e057cbd26db39b0d1d0988e76d9e
BrianKip2000 <kipchirchirbrian559@gmail.com>
2024-04-08 23:53:27 +0300

    suare init size

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-#Define a Square class
+"""Define a Square class"""
+
 
 class Square:
+    """Size initialized using __init__"""
     def __init__(self, size):
         self.__size = size
