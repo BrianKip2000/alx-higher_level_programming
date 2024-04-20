308d6a174cc75f21aca9ce9667243eb6b829a124
BrianKip2000 <kipchirchirbrian559@gmail.com>
2024-04-20 06:14:15 +0300

    Singly Linked List

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -2,35 +2,42 @@
 "Creating a singly linked list node"
 
 class Node:
+    "Class Node"
     def __init__(self, data, next_node=None):
         self.data = data
         self.next_node = next_node
 
     @property
     def data(self):
+        """Getter"""
         return self.__data
 
     @data.setter
     def data(self, value):
+        """Setter"""
         if not isinstance(value, int):
             raise TypeError("data must begin with an integer")
         self.__data = value
     
     @property
     def next_node(self):
+        """Getter"""
         return self.__next_node
 
     @next_node.setter
     def next_node(self, value):
+        """Setter"""
         if value is not None and not isinstance(value, Node):
             raise TypeError("next_node must be a Node object or None")
         self.__next_node = value
 
 class SinglyLinkedList:
+    """Class Singly Linked List"""
     def __init__(self):
         self.head = None
 
     def sorted_insert(self, value):
+        """Sorting insert"""
         new_node = Node(value)
         if self.head is None:
             self.head = new_node
@@ -49,6 +56,7 @@ class SinglyLinkedList:
         current.next_node = new_node
 
     def __str__(self):
+        """Making it a string"""
         result = ""
         current = self.head
         while current:
