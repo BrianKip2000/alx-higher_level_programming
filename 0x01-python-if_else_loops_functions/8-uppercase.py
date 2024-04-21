b2e560f23c26834e25ac48beff1b914614f8a042
BrianKip2000 <kipchirchirbrian559@gmail.com>
2024-04-21 09:26:40 +0300

    uppercase string

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def uppercase(s):
+    """Uppercase:"""
+    for char in s:
+        uppercase_char = chr(ord(char) - 32) if 'a' <= char <= 'z' else char
+        print(uppercase_char, end="")
+    print()
