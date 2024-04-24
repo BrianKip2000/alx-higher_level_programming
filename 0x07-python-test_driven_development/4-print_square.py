10862a35277d147342847859e8fca67052618d08
BrianKip2000 <kipchirchirbrian559@gmail.com>
2024-04-24 20:12:04 +0300

    Size of square

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""Printing square size"""
+
+def print_square(size):
+    """Funtion module to print size of square using #"""
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    if (size is float and size < 0):
+        raise TypeError("size msut be an integer")
+    for i in range(size):
+        for j in range(size):
+            print("#",end="")
+        print()
