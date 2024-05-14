2edd15145c8060201cdba290ca4249bf8278c936
BrianKip2000 <kipchirchirbrian559@gmail.com>
2024-05-14 20:13:01 +0300

    subclasses

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -9,4 +9,4 @@ def inherits_from(obj, a_class):
     obj: anytype
     a_class: class containing values
     """
-    return issubclass(type(obj), a_class) and type(obj) != a_class
+    return issubclass(type(obj), a_class) and type(obj) is not a_class
