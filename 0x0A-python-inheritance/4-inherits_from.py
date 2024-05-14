e6c12aae0a0822514d7ba83e580532ee208d9b60
BrianKip2000 <kipchirchirbrian559@gmail.com>
2024-05-14 20:10:15 +0300

    issubclass

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -2,11 +2,11 @@
 """Module to check instance of a class"""
 
 
-def is_kind_of_class(obj, a_class):
+def inherits_from(obj, a_class):
     """
     Function to check for instance of class
     Args:
     obj: anytype
     a_class: class containing values
     """
-    return isinstance(obj, a_class)
+    return issubclass(type(obj), a_class) and type(obj) != a_class
