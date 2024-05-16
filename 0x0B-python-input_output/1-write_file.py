fd7c4aa5fa513132deada8188f6b5430b895ebfc
BrianKip2000 <kipchirchirbrian559@gmail.com>
2024-05-16 05:45:46 +0300

    lenText

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -6,5 +6,5 @@ def write_file(filename="", text=""):
     """Function to write file using write()"""
     with open(filename, mode='w', encoding='UTF-8') as file1:
         file1.write(text)
-        return len(text)
+        print(len(text))
     
