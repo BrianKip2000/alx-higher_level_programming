adb582e8abab3e107b1813af2d63c646060205cb
BrianKip2000 <kipchirchirbrian559@gmail.com>
2024-05-16 17:35:23 +0300

    technical interview

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Module to find pascal triangle"""
+
+
+def pascal_triangle(n):
+    # Initialize triangle list to store rows
+    triangle = []
+
+
+    if n <= 0:
+        return triangle
+
+    for i in range(n):
+        row = []
+
+        for j in range(i + 1):
+            coefficient = factorial(i) // (factorial(j) * factorial(i - j))
+            row.append(coefficient)
+
+        triangle.append(row)
+
+    return triangle
+
+
+def factorial(num):
+    """Factorial helper function"""
+    result = 1
+    for i in range(1, num + 1):
+        result *= i
+    return result
