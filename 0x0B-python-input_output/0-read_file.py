ddcdacd0e060da2e8351384931184d83a53eaac6
BrianKip2000 <kipchirchirbrian559@gmail.com>
2024-05-16 05:17:08 +0300

    read file using read()

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
+"""Module to read a file using read()"""
 
 
 def read_file(filename=""):
+    """
+    A function that reads the content of a file and prints it out.
+    Parameters:
+        filename (str): The name of the file to be read. Default is an empty string.
+    Returns:
+        None
+    """
     with open("my-file_0.txt", mode="r", encoding="UTF-8") as UTF8:
         read_file = UTF8.read()
 
