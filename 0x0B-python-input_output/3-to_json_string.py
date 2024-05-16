594f0b31217018d89d3e4ad934380c30dc5c3bc7
BrianKip2000 <kipchirchirbrian559@gmail.com>
2024-05-16 06:17:40 +0300

    json string

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import json
+"""Json representation of an object"""
+
+
+def to_json_string(my_obj):
+    """Converting to json object(string) using dumps""" 
+    return json.dumps(my_obj)
