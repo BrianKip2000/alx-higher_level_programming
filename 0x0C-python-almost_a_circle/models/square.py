dc44fa3508c5a929434925fd691b4981edc3d676
BrianKip2000 <kipchirchirbrian559@gmail.com>
2024-05-20 20:10:33 +0300

    Setting with width equal to height

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -32,6 +32,6 @@ class Square(Rectangle):
         if type(value) is not int:
             raise TypeError(f"width must be an integer")
         if value <= 0:
-            raise ValueError("width must be >= 0")
+            raise ValueError("width must be > 0")
         self.width = value
         self.height = value
