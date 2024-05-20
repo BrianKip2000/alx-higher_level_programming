503a56435e6e36d323b8c037b19a98c588735ebc
BrianKip2000 <kipchirchirbrian559@gmail.com>
2024-05-20 18:33:41 +0300

    Correction of square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -5,9 +5,16 @@ from models.rectangle import Rectangle
 class Square(Rectangle):
     """Class that takes its properties from rectangle"""
     def __init__(self, size, x=0, y=0, id=None):
+        """
+        Initializes Square class instance:
+        Args:
+            size (int): size of the square equal to rect dim
+            x (any): positional argument
+            y (any): positional argument
+            id : unique id
+        """
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
         """String representation of the class"""
         return (f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}")
-    
