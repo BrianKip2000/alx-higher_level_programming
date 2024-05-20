8ee4ea28a03dc5ba6ac59176b0ba60e5c8afc247
BrianKip2000 <kipchirchirbrian559@gmail.com>
2024-05-20 19:33:31 +0300

    Why dows not whok

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,45 @@
+<<<<<<< HEAD
+#!/usr/bin/python3
+"""module for creating a square based on rect"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Class that takes its properties from rectangle"""
+    def __init__(self, size, x=0, y=0, id=None):
+        """
+        Initializes Square class instance:
+        Args:
+            size (int): size of the square equal to rect dim
+            x (any): positional argument
+            y (any): positional argument
+            id : unique id(str)
+        """
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """String representation of the class"""
+        return (f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}")
+=======
+#!/usr/bin/python3
+from models.rectangle import Rectangle
+"""module for creating a square based on rect"""
+
+
+class Square(Rectangle):
+    """Class that takes its properties from rectangle"""
+    def __init__(self, size, x=0, y=0, id=None):
+        """
+        Initializes Square class instance:
+        Args:
+            size (int): size of the square equal to rect dim
+            x (any): positional argument
+            y (any): positional argument
+            id : unique id(str)
+        """
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """String representation of the class"""
+        return (f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}")
+>>>>>>> a805aa0f095d5b600ce36b5a69dea8524bc30ec4
