16fff3ab508d87d903961fefd33f56d8ec2c35f0
BrianKip2000 <kipchirchirbrian559@gmail.com>
2024-05-22 16:50:03 -0500

    Show create table for printing table

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,7 +1,2 @@
 -- Showing details of a specific table
-USE hbtn_0c_0;
-
-SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, CHARACTER_SET, COLLATION, EXTRA
-FROM information_schema.columns
-WHERE table_name = 'first_table'
-ORDER BY ORDINAL_POSITION;
+SHOW CREATE TABLE first_table;
