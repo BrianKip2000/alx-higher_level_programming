b37c6a4c02b47b91618fb9467d39dc54882647e5
BrianKip2000 <kipchirchirbrian559@gmail.com>
2024-05-22 16:46:39 -0500

    hhg

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,13 +1,7 @@
 -- Showing details of a specific table
-SELECT
-	COLUMN_NAME,
-	COLUMN_TYPE,
-	IS_NULLABLE,
-	COLUMN_KEY,
-	COLUMN_DEFAULT,
-	EXTRA
-FROM
-	information_schema.COLUMNS
-WHERE
-	TABLE_SCHEMA = 'hbtn_0c_0'
-	AND TABLE_NAME = 'first_table'
+USE hbtn_0c_0;
+
+SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, CHARACTER_SET, COLLATION, EXTRA
+FROM information_schema.columns
+WHERE table_name = 'first_table'
+ORDER BY ORDINAL_POSITION;
