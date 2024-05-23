04e19d776d889f693b5efc9e07413e7cf9def0e2
BrianKip2000 <kipchirchirbrian559@gmail.com>
2024-05-23 23:20:21 +0300

    Group by and order by

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- List tables with same values
-SELECT score, COUNT(*) AS numbers FROM second_table GROUP BY score DESC
+SELECT score, COUNT(*) AS numbers FROM second_table GROUP BY score ORDER BY numbers DESC;
