57069221de638e56a3db42f920a61bb9cbd5a0a7
BrianKip2000 <kipchirchirbrian559@gmail.com>
2024-05-23 23:56:38 +0300

    Yes!

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- List tables with same values
-SELECT score, COUNT(score) AS numbers FROM second_table GROUP BY score ORDER BY score DESC;
+SELECT score, COUNT(score) AS number FROM second_table GROUP BY score ORDER BY score DESC;
