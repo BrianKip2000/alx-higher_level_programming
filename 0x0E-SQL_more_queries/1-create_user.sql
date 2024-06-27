063304d284d78cdaed6dc96735aaf2eb3baf16f1
BrianKip2000 <kipchirchirbrian559@gmail.com>
2024-06-27 11:05:45 +0300

    create a user

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- Create a user called user_0d_1 
+CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+-- Grant all priveledges on user_0d_1
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
