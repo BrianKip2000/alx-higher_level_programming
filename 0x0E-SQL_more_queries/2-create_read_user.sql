ba2158a8e5233d7c21c7bfec10a7f3ba2104f2fe
BrianKip2000 <kipchirchirbrian559@gmail.com>
2024-07-04 02:04:06 +0300

    Create a user

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -3,5 +3,5 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 -- Create a user called user_0d_2
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED WITH mysql_native_password BY 'user_0d_2_pwd';
 -- Grant select on the user
-GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost' WITH GRANT OPTION;
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
 FlUSH PRIVILEGES;
