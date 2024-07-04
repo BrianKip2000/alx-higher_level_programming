92b96aff452ca4434142e478d97af70391d10a25
BrianKip2000 <kipchirchirbrian559@gmail.com>
2024-07-04 20:25:19 +0300

    We are winning

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,2 +1,3 @@
 -- List all shows in the database
-SELECT sh.title, ge.genre_id FROM tv_shows sh LEFT JOIN tv_show_genres ge ON sh.id=ge.genre_id ORDER BY sh.title, ge.genre_id;
+-- SELECT sh.title, ge.genre_id FROM tv_shows sh LEFT JOIN tv_show_genres ge ON sh.id=ge.genre_id ORDER BY sh.title, ge.genre_id;
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
