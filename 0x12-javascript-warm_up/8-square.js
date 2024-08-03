3c1461d5b884533fa5c888f1361c67006db3db16
BrianKetubo <kipchirchirbrian559@gmail.com>
2024-08-03 15:38:41 -0700

    This is a square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 const a = Math.floor(Number(process.argv[2]));
 if (isNaN(a)) {
-  console.log("Missing size");
+  console.log('Missing size');
 } else {
   for (let i = 0; i < a; i++) {
-	  let c  = ''
-	  for (let j = 0; j < a; j++ ) c += 'X';
-		  console.log(c); 
+    let c = '';
+    for (let j = 0; j < a; j++) c += 'X';
+    console.log(c);
   }
 }
