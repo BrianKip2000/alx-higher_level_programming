af7b6b87fab1c774e4cdf85e2813a17776c53732
Ketubo <kipchirchirbrian75@gmail.com>
2024-08-03 22:02:49 +0300

    Print the first argument

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,9 +1,3 @@
 #!/usr/bin/node
 // Prints the first argument passed to it
-const { argv } = require('node:process');
-
-if (argv[2] === undefined) {
-  console.log('No argument');
-} else {
-  console.log(argv[2]);
-}
+console.log(typeof process.argv[2] === 'undefined' ? 'No argument' : process.argv[2]);
