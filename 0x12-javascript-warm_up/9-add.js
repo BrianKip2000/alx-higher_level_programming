d6f0ee8e53663bfc1d7ea1bbf22cff5b8a05a5e9
BrianKetubo <kipchirchirbrian559@gmail.com>
2024-08-03 17:52:21 -0700

    Add two numbers

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
-function add(a, b) {
+
+function add (a, b) {
   return a + b;
 }
 
-console.log(add(Math.floor(Number(process.argv[2])),Math.floor(Number( process.argv[3]))));
+console.log(add(Math.floor(Number(process.argv[2])), Math.floor(Number(process.argv[3]))));
