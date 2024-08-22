4e6a9420ccc74dadd907bacbc0b666faca060510
BrianKip2000 <kipchirchirbrian559@gmail.com>
2024-08-22 14:03:23 +0300

    Retrieve all instances of states ordered by id

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+    """Takes in db name, password, mysql username"""
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+
+    #connecting to db
+    db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=mysql_username,
+            password=mysql_password,
+            db=database_name
+            )
+
+    #cursor object creation
+    cur=db.cursor()
+
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+    
+
+    cur.close()
+    db.close()
+
